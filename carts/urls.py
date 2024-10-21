bae6113db09c0c3c5ab6d5ddd4296f16b433733d
Nhan Nguyen Van Khanh <nhan.nguyenvan@paradox.ai>
2024-10-21 15:02:52 +0700

    refactor(carts): refactor url for consistence
    - cart/add/ -> cart/items/
    - cart/remove/<int:product_id> -> cart/items/<int:product_id>

--- carts/urls.py
+++ carts/urls.py
@@ -3,11 +3,9 @@ from django.urls import path
 from carts.views import AddToCartView, GetCartView, RemoveFromCartView
 
 urlpatterns = [
-    path("", GetCartView.as_view(), name="get_cart"),
-    path("add/", AddToCartView.as_view(), name="add_to_cart"),
+    path("", GetCartView.as_view(), name="get-cart"),
+    path("items/", AddToCartView.as_view(), name="add-to-cart"),
     path(
-        "remove/<int:product_id>/",
-        RemoveFromCartView.as_view(),
-        name="remove_from_cart",
+        "items/<int:product_id>/", RemoveFromCartView.as_view(), name="remove-from-cart"
     ),
 ]
